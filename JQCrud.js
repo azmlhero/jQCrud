e3ba63f8ee2f5c58109a46faa1678cfa4fdae20d
ammar <azml1@gmail.com>
2021-03-27 04:23:43 +0500

    A

--- JQCrud.js
+++ JQCrud.js
@@ -1,9 +1,46 @@
 $(function(){
 loadRecipies();
 $("#recipes").on("click",".btn-danger",handleDelete);
+$("#recipes").on("click",".btn-warning",handleUpdate);
 $("#addBtn").click(addRecipe);
+$("#updateSave").click(function(){
+   var id = $("updateId").val();
+   var title = $("updateTitle").val();
+   var body = $("updateBody").val();
+    $.ajax({
+    
+        url:"https://usman-recipes.herokuapp.com/api/recipes/" +id,
+        data:{title,body},
+        method:"PUT",
+        success:function (response){
+            console.log(response);
+            loadRecipies(); 
+
+            $("#updateModal").modal("hide");
+
+        }
+    })
+})
 });
 
+function handleUpdate(){
+    var btn= $(this);
+    var parentDiv =btn.closest(".recipe");
+    let id =parentDiv.attr("data-id");
+    $.get("https://usman-recipes.herokuapp.com/api/recipes/"+id,function(response){
+        $("updateId").val(response._id);
+        
+        $("updateTitle").val(response.title);
+
+        $("updateBody").val(response.body);
+
+        $("#updateModal").modal("show");
+
+        
+
+    })
+    
+}
 function addRecipe(){
     var title=$("#title").val();
     var body = $("#body").val();
@@ -55,8 +92,9 @@ function  loadRecipies(){
 
                 $("#recipes").append(`
                 
-                <div class ="recipe" data-id="${rec._id}"> <h3>${rec.title}</h3> <p><button class="btn btn-danger btn-sm float-right">DELETE</button><button class="btn btn-warning btn-sm float-right">Edit</button>${rec.body}</p></div>
+                <div class ="recipe" data-id="${rec._id}"> <h3>${rec.title}</h3> <p><button class="btn btn-danger btn-sm float-right">DELETE</button><button type="button" class="btn btn-warning btn-sm float-right"  data-toggle="modal" data-target="#updateModal">Edit</button>${rec.body}</p></div>
                 `);
+                 
 
                 // $("#recipes").append("<div><h3>"+rec.title+"</h3></div>");
 
