fecc07006de6f4cf1182bb8e343f7daca2bd0d4e
ammar <azml1@gmail.com>
2021-04-13 14:13:30 +0500

    Update JQCrud.js

--- JQCrud.js
+++ JQCrud.js
@@ -1,10 +1,10 @@
 $(function(){
 loadProducts();
 $("#products").on("click",".btn-danger",handleDelete);
+$("#btn").click(addProduct);
 
 $("#products").on("click",".btn-warning",handleUpdate);
 // glt nam sayy call kar raha tha jo function hay  uska P capital hay
-$("#btn").click(addProduct);
 
 $("#save").click(function(){
 
@@ -96,38 +96,38 @@ function handleUpdate(){
     } );
     
 }
-function addProduct(){
-    $("#addModal").modal("show");
+// function addProduct(){
+//     $("#addModal").modal("show");
     
 //     }
-// function addProduct(){
-// //       var name = $("#Name").val();
-// //    var price = $("#Price").val();
-// //    var color = $("#Color").val();
-// //    var department = $("#Department").val();
-// //    var description =$("#Description").val();
+function addProduct(){
+      var name = $("#Name").val();
+   var price = $("#Price").val();
+   var color = $("#Color").val();
+   var department = $("#Department").val();
+   var description =$("#Description").val();
 
     
-// //     $.ajax({
-// //         url:"https://usman-recipes.herokuapp.com/api/products/",
-// //         method:"POST",
-// //         data:{name,price,color,department,description},
-// //         success: function(response){
-// //             console.log(response);
-// //             // icko jb say set kia add nai ho raha bs console ya network pay hota hay list may nai ho raha hay update ka b masla hay
+    $.ajax({
+        url:"https://usman-recipes.herokuapp.com/api/products/",
+        method:"POST",
+        data:{name,price,color,department,description},
+        success: function(response){
+            console.log(response);
+            // icko jb say set kia add nai ho raha bs console ya network pay hota hay list may nai ho raha hay update ka b masla hay
             
-// //              $("#Name").val("");
-// //             $("#Price").val("");
-// //              $("#Color").val("");
-// //              $("#Department").val("");
-// //             $("#Description").val("");
+             $("#Name").val("");
+            $("#Price").val("");
+             $("#Color").val("");
+             $("#Department").val("");
+            $("#Description").val("");
          
-// //             // $("#title").val("");
-// //             // $("#body").val("");
+            // $("#title").val("");
+            // $("#body").val("");
         
-// //             loadProducts();
-//             $("#addModal").modal("show");
-//         }
+            loadProducts();
+            $("#addModal").modal("show");
+        }
   
 function handleDelete(){
     var btn= $(this);
